e4bf5237d7183f52c5492a788a648d70da275a01
kskrao219 <keshava.balaji@anywhere.co>
2022-11-11 01:08:25 +0530

    added react app

---
+++ React-redux-app/application/src/components/CakeContainer.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const CakeContainer = () => {
+    return(
+        <>
+        <div>
+            <h2>No of cakes </h2>
+            <button> Buy Cake</button>
+        </div>
+        </>
+    )
+}
+export default CakeContainer;
